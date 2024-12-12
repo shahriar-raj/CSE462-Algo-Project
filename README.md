3a92c39b89d07d9a9d14cc1493dc5378af8a8b9e
Md. Nafiu Rahman <nafiu.rahman@gmail.com>
2024-12-12 17:36:43 +0600

    files arranged

---
+++ README.md
@@ -0,0 +1,2 @@
+# CSE462-Project
+ 
--- run.sh
+++ run.sh
@@ -1,20 +1,23 @@
 c=1
+
+g++ dreyfus-wagner.cpp -O3 -o dreyfus-wagner
+
 for x in `ls testcases`
 do  
     # if c < 14 continue
-    if [ $c -lt 27 ]
+    if [ $c -lt 14 ]
     then
         c=$((c + 1))
         continue
     fi
     echo $c
     start=$(date +%s%N)
-    p=`./stapp < "testcases/$x"`
+    p=`./dreyfus-wagner < "testcases/$x"`
     end=$(date +%s%N)
     input_file="testcases/$x"
-    output_file="output2.csv"
+    output_file="output3.csv"
     echo -n "$c," >> "$output_file"
     awk 'NR==2 {printf "%s,", $2} NR==3 {printf "%s,", $2} /Terminals / {printf "%s,", $2}' "$input_file" >> "$output_file"
     echo "$(($(($end-$start))/1000000)) ms" >> "$output_file"
     c=$((c + 1))
-done
+done 
